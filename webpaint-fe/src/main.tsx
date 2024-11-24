ca6d2421ab8fe8d2765de922b4de1eccc381240e
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-11-23 21:05:22 -0600

    css reset 추가 및 react router 라이브러리 추가.

--- webpaint-fe/src/main.tsx
+++ webpaint-fe/src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from "react";
+import "./main.css";
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
